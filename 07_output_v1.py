87116ccedef95e28fe40c7c8fe0b7ddc41c32541
WronglyInspired <wonge2@middleton.school.nz>
2023-05-15 09:36:49 +1200

    Create 07_output_v1.py

---
+++ 07_output_v1.py
@@ -0,0 +1,58 @@
+"""Component 07: Output cards
+
+Prints all cards to catalogue, giving option for user to print:
+ Just names:
+ Stoneling
+
+ or Full info:
+ **Stoneling**
+ Strength: 7
+ Speed: 1
+ Stealth: 25
+ Cunning: 15
+"""
+
+from easygui import *
+
+TK = ["Strength", "Speed", "Stealth", "Cunning"]  # Trait Key
+
+MAX_TRAIT, MIN_TRAIT = 25, 1  # maximum and minimum values for traits
+
+card_cat = [  # card catalogue
+    ["Stoneling", [7, 1, 25, 15]],
+    ["Vexscream", [1, 6, 21, 19]],
+    ["Dawnmirage", [5, 15, 18, 22]],
+    ["Blazegolem", [15, 20, 23, 6]],
+    ["Websnake", [7, 15, 10, 5]],
+    ["Moldvine", [21, 18, 14, 5]],
+    ["Vortexwing", [19, 13, 19, 2]],
+    ["Rotthing", [16, 7, 4, 12]],
+    ["Froststep", [14, 14, 17, 4]],
+    ["Wispghoul", [17, 19, 3, 2]],
+]
+
+
+def format_card(card):
+    """Returns card details as a str"""
+    msg = f"**{card[0]}**\n"
+    for index, trait in enumerate(card[1]):
+        msg += f" {TK[index]}: {trait}\n"
+    return msg
+
+
+def output_cards(cat=card_cat):
+    title = "Output Cards"
+    choice = buttonbox("What would you like to print?", title,
+                       choices=["Names only", "Full info", "Cancel"])
+    if choice == "Cancel":
+        return
+    print("#### MONSTER CARD CATALOGUE ####")
+    print(f" Total cards: {len(cat)}\n")
+    for card in cat:
+        if choice == "Names only":
+            print(card[0])
+        elif choice == "Full info":
+            print(format_card(card))
+
+
+output_cards(card_cat)
